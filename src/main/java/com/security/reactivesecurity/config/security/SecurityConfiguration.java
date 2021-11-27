eb309931420cb039a55976ea690a165afa88736f
Chetan Jangid <chetan.windowslive@outlook.com>
2021-11-27 22:28:15 +0530

    Removed unused import

--- src/main/java/com/security/reactivesecurity/config/security/SecurityConfiguration.java
+++ src/main/java/com/security/reactivesecurity/config/security/SecurityConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
